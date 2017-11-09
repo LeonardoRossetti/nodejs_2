8733bd8ee63382671561b0541cabb07df8a6ea19
LeonardoRossetti <leoo.rossetti@gmail.com>
2017-11-09 07:24:18 -0200

    Alterações

--- noticias.js
+++ noticias.js
@@ -7,7 +7,16 @@ http.createServer( function(req, res){
 }).listen(3000);*/
 
 var server = http.createServer( function(req, res){
-    res.end('<html><body>Portal de notícias</body></html>');
+
+    var categoria = req.url;
+
+    if(categoria == '/tecnologia'){
+        res.end('<html><body>Tecnologia</body></html>');
+    } else if(categoria == '/games'){
+        res.end('<html><body>Games</body></html>');
+    } else{
+        res.end('<html><body>Portal de notícias</body></html>');
+    }
 });
 
 server.listen(3000);
