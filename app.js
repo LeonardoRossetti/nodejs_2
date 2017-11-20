4bbf24aca5f7d7c2ca93e443edd3df37a58d5caa
LeonardoRossetti <leoo.rossetti@gmail.com>
2017-11-20 07:39:42 -0200

    Create folders admin, home and noticias in folder views and create the call for them

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ var app = express(); //executa a função que o modulo express retorna
 
 app.set('view engine', 'ejs'); //agora o express sabe que o ejs será a nossa engine de views
 
-app.get('/tecnologia', function(req, res){ //feito chamada para a pagina principal
-    res.render("secao/tecnologia");
-})
-
 app.get('/', function(req, res){ //feito chamada para a pagina principal
-    res.send("<html><body>Portal de notícias</body></html>");
+    res.render("home/index");
 })
 
+app.get('/inclusao_noticia', function(req, res){
+    res.render("admin/form_add_noticia");
+})
+
+app.get('/noticias', function(req, res){ 
+    res.render("noticias/noticias");
+})
 
 app.listen(3000, function(){
     console.log('Servidor rodando com Express na porta 3000!');
