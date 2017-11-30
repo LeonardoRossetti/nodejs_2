89dc472fd8c6cc2702a28ee5f917b8f7dec38b11
LeonardoRossetti <leoo.rossetti@gmail.com>
2017-11-30 07:36:27 -0200

    Criada a conexão com a base de dados

--- app/routes/news.js
+++ app/routes/news.js
@@ -1,4 +1,14 @@
 module.exports = function(app){ 
+
+    var mysql = require('mysql');
+
+    var connection  = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '12345678',
+        database: 'portal_noticias'
+    });
+
     app.get('/news', function(req, res){ 
         res.render("news/news");
     });
