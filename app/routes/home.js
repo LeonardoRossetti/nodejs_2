6a56d94829982a31d3fe6062dc66d89585072636
LeonardoRossetti <leoo.rossetti@gmail.com>
2017-12-06 06:47:17 -0200

    Simple changes into news

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,11 +1,6 @@
 module.exports = function(app) {
     
     app.get('/', function(req, res){ //feito chamada para a pagina principal
-
-        var connection = app.config.dbConnection();
-
-        connection.query('select * from noticias', function(error, result){ 
-            res.render("home/index", {content: result});
-        })
+        res.render("home/index", {content: result});
     });
 };
