825e54da0c2aa4b50e49adcc847e3524998e5757
LeonardoRossetti <leoo.rossetti@gmail.com>
2017-12-08 13:00:57 -0200

    Create a method to insert new notice

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,17 @@
-module.exports = function(app) {
-    app.get('/include_news', function(req, res){
+module.exports = function(application) {
+    application.get('/include_news', function(req, res){
         res.render("admin/form_add_notice");
     });
 
-    app.post('/noticias/salvar', function(req, res){
+    application.post('/noticias/salvar', function(req, res){
         var noticia = req.body;
-        res.send(noticia);
+        
+        var connection = application.config.dbConnection();
+        var newsModel = application.app.models.newsModel;
+    
+        newsModel.saveNotice(noticia, connection, function(error, result){
+            //res.render("news/news", {news: result});
+            res.redirect('/news');
+        });
     });
 };
