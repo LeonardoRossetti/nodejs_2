570cd2902f173c79480ae3d0659407614fb4c1a3
LeonardoRossetti <leoo.rossetti@gmail.com>
2017-12-13 07:23:14 -0200

    Adjusting the class NewsDAO

--- app/models/newsDAO.js
+++ app/models/newsDAO.js
@@ -2,20 +2,20 @@ function NewsDAO(connection){
     this._connection = connection;
 }
 
-NewsDAO.prototype.getNews = function(callback) {
+NewsDAO.prototype.getNews = function(callback){
     this._connection.query("select * from noticias", callback);
 };
 
-NewsDAO.prototype.getNotice = function(callback) {
+NewsDAO.prototype.getNotice = function(callback){
     this._connection.query("select * from noticias where id_noticia = 1", callback);
 };
 
-NewsDAO.prototype.saveNotice = function(notice, callback) {
+NewsDAO.prototype.saveNotice = function(notice, callback){
     //Para que isso funcione é necessário que o rótulo das 
     //variáveis tenha o mesmo nome que as colunas da tabela
     this._connection.query('INSERT INTO NOTICIAS SET ? ', notice, callback);
 };
 
-module.exports = function() {
+module.exports = function(){
     return NewsDAO;
 };
