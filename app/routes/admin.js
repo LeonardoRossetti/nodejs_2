762c79e087f1a43c0d439b0b49fc875b11922c86
LeonardoRossetti <leoo.rossetti@gmail.com>
2017-12-14 08:01:36 -0200

    Created a validation using express-validators

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,12 +5,25 @@ module.exports = function(application) {
 
     application.post('/noticias/salvar', function(req, res){
         var noticia = req.body;
+
+        //using express-validator
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres.').len(10, 100);
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatória').notEmpty();
+        req.assert('noticia', 'Noticia é obrigatória').notEmpty();
+        
+        var erros = req.validationErrors();
+        if (erros) {
+            res.render("admin/form_add_notice");
+            return;
+        }        
         
         var connection = application.config.dbConnection();
         var newsDAO = new application.app.models.NewsDAO(connection);
     
         newsDAO.saveNotice(noticia, function(error, result){
-            //res.render("news/news", {news: result});
             res.redirect('/news');
         });
     });
