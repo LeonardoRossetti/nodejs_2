55fdd785e568e4e6a7d5f3229c202c8a6e44e8a8
LeonardoRossetti <leoo.rossetti@gmail.com>
2017-12-15 12:51:40 -0200

    Put notice into news, unifying the files

--- app/routes/news.js
+++ app/routes/news.js
@@ -11,4 +11,14 @@ module.exports = function(application){
             res.render("news/news", {news: result});
         });
     });
+
+    application.get('/notice', function(req, res){ 
+        
+        var connection = app.config.dbConnection();
+        var newsDAO = new app.app.models.NewsDAO(connection);
+
+        newsDAO.getNotice(function(err, result){
+            res.render("news/notice", {notice: result});
+        });
+    });
 };
