139aa6ce7109de72c7af4c9832179c654a714276
LeonardoRossetti <leoo.rossetti@gmail.com>
2017-12-15 12:53:19 -0200

    Adjusting

--- app/routes/news.js
+++ app/routes/news.js
@@ -14,8 +14,8 @@ module.exports = function(application){
 
     application.get('/notice', function(req, res){ 
         
-        var connection = app.config.dbConnection();
-        var newsDAO = new app.app.models.NewsDAO(connection);
+        var connection = application.config.dbConnection();
+        var newsDAO = new application.app.models.NewsDAO(connection);
 
         newsDAO.getNotice(function(err, result){
             res.render("news/notice", {notice: result});
